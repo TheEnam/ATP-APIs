b6a7fb07c957df26d1f1cac19f4ac3ed2b07215b
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2025-04-17 09:19:07 +0000

    fixing cors error again

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(
     cors({
-        origin: [APP_ORIGIN,'https://atp-apis.onrender.com'],
+        origin: [APP_ORIGIN],
         credentials: true,
     })
 );
