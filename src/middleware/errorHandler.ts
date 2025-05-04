83651ffb1d037cd497c746c436100836375065ba
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2025-05-04 15:39:14 +0000

    fix: response body

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -5,6 +5,7 @@ import { Request, Response,NextFunction } from "express";
 import { z } from "zod";
 import AppError from "../utils/AppError";
 import { clearAuthCookies, REFRESH_PATH } from "../utils/cookies";
+import { Error as MongooseError } from "mongoose";
 
 const handleZodError = (res: Response, error: z.ZodError) => {
     const errors = error.issues.map((err) => ({
@@ -22,24 +23,38 @@ const handleAppError = (res: Response, error: AppError) => {
     errorCode: error.errorCode});
 };
 
-const errorHandler: ErrorRequestHandler= (error,req,res,next) => {
-    console.log(`PATH: ${req.path}`,error);
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+    console.log(`PATH: ${req.path}`, error);
 
-    if(req.path === REFRESH_PATH)
-    {
-      clearAuthCookies(res);
+    if (req.path === REFRESH_PATH) {
+        clearAuthCookies(res);
     }
-    if(error instanceof z.ZodError){
-      handleZodError(res,error);
-        
-    }
-
-    if(error instanceof AppError){
-      handleAppError(res,error);
+    if (error instanceof z.ZodError) {
+        handleZodError(res, error);
+        return;
     }
 
+    if (error instanceof AppError) {
+        handleAppError(res, error);
+        return;
+    }
+
+    if (error instanceof MongooseError.ValidationError) {
+        const errors = Object.values(error.errors).map(error => ({
+            field: error.path,
+            message: error.message
+        }));
+
+        res.status(BAD_REQUEST).json({
+            status: 'error',
+            message: 'Validation Error',
+            errors
+        });
+        return;
+    }
 
     res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
+    return;
 };
 
 export default errorHandler;
